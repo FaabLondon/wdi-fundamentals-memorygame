e1eb8a97947ef96d6113a21fbc352aa4fe6759f6
Fabienne Prouteau <faabke@gmail.com>
2017-12-30 15:21:42 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards =["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne;
+var cardTwo;
+
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
 console.log("User flipped: " + cardOne);
-console.log("Uder flipped: " + cardThree);
+
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped: " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("You found a match!")
+	}
+	else
+	{
+		alert("Sorry, try again!")
+	}
+}
